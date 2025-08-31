f047c400400ddde6333eb9811a1172f06dc1cf48
greemaltend4 <dengestabiy@hotmail.com>
2025-08-31 10:00:40 -0500

    Fix typo in welcome message to improve readability.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue dans l application d intégration cloud gestionnaire!');
+  res.send('Bienvenue dans l application d intégration cloud gestionnaire !');
 });
 
 app.listen(port, () => {
